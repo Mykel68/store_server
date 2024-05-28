ac3bd0c6b0e509e2eb8dcbe93cf821ff266c34b7
mykel68 <olowookeremicheal9568@gmail.com>
2024-05-28 17:48:06 +0100

    added export to the product route

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -2,3 +2,5 @@ const router = require("express").Router();
 const authController = require("../controllers/productController");
 
 router.post("/register", authController.addProducts);
+
+module.exports = router;
